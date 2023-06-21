e6a43d4d54663559c7a7e81c76474307fe3fe091
Jeffrey <562475805lanu@gmail.com>
2023-06-21 20:27:07 +1000

    fix errors, add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 function App() {
   return <div>Hello vite</div>;
 }
