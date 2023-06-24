8a4e00fd8d29040c3af7de3e4478fe8dbfb904b8
Jeffrey <562475805lanu@gmail.com>
2023-06-24 11:12:11 +1000

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
   {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+    ],
   },
 ]);
 
