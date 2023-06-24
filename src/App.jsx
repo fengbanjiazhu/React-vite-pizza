659bc9439b0271b9aae480bd52c34c5578af5de1
Jeffrey <562475805lanu@gmail.com>
2023-06-24 10:56:36 +1000

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
 
-createBrowserRouter([
+const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   {
     path: "/menu",
     element: <Menu />,
   },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
 ]);
 
 function App() {
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
