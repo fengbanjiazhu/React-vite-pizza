35d0f71b16b77dc576bc656322146003b40627ae
Jeffrey <562475805lanu@gmail.com>
2023-06-25 17:20:54 +1000

    search data & fetch data from online api

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 
@@ -25,6 +26,12 @@ const router = createBrowserRouter([
       {
         path: "/order/new",
         element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
