aaf9451a180da64659b9bffdccc88b1990d88756
Jeffrey <562475805lanu@gmail.com>
2023-06-28 18:24:48 +1000

    Adding grid and flex

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Username() {
-  return <div className="hidden text-sm font-semibold">Jeff</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jeff</div>;
 }
 
 export default Username;
