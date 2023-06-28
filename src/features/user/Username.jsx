b2d7c9ecbde5650b875def72c407cbe5a3d6948c
Jeffrey <562475805lanu@gmail.com>
2023-06-28 17:44:06 +1000

    add styling

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Username() {
-  return <div className="text-sm font-semibold">Jeff</div>;
+  return <div className="hidden text-sm font-semibold">Jeff</div>;
 }
 
 export default Username;
